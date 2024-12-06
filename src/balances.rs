37fb4fc4faa9ecdbd299e22a9b80ccfb024b288d
unpeturbed <ayodeledrust@gmail.com>
2024-12-06 18:32:13 +0100

    Initial Commit

---
+++ src/balances.rs
@@ -0,0 +1,34 @@
+use std::collections::BTreeMap;
+pub struct Pallet {
+	balances: BTreeMap<String, u128>,
+}
+
+impl Pallet {
+	pub fn new() -> Self {
+		Self { balances: BTreeMap::new() }
+	}
+
+	pub fn set_balance(&mut self, who: &String, amount: u128) {
+		self.balances.insert(who.clone(), amount);
+	}
+
+	pub fn balance(&self, who: &String) -> u128 {
+		*self.balances.get(who).unwrap_or(&0)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+
+	#[test]
+	fn init_balances() {
+		use super::*;
+
+		let mut balances = Pallet::new();
+
+		assert_eq!(balances.balance(&"alice".to_string()), 0);
+		balances.set_balance(&"alice".to_string(), 100);
+		assert_eq!(balances.balance(&"alice".to_string()), 100);
+		assert_eq!(balances.balance(&"alic".to_string()), 0);
+	}
+}
---
+++ src/main.rs
@@ -0,0 +1,3 @@
+mod balances;
+
+fn main() {}
